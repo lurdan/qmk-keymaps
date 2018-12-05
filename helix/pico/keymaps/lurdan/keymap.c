2bac99d3d6f369b684190aadba09bbcaafad6255
KURASHIKI Satoru <lurdan@gmail.com>
2018-12-05 11:24:44 +0900

    Update keymap.c

--- helix/pico/keymaps/lurdan/keymap.c
+++ helix/pico/keymaps/lurdan/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_YEN, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_UNDS, \
-      ADJUST, RAISE,  KC_LGUI, KC_LALT,   CTL_ESC, KC_BSPC,TAB_LOWR,ENT_RAIS, KC_SPC, KC_RALT,  KC_RCTL, LOWER,   _______, _______ \
+      ADJUST, RAISE,  KC_LGUI, KC_RALT,   CTL_ESC, KC_BSPC,TAB_LOWR,ENT_RAIS, KC_SPC, KC_RALT,  KC_RCTL, LOWER,   _______, _______ \
       ),
 
   /* Eucalyn
