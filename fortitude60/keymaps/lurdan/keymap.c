3a8b8428a36e632c1bcb9418f1d09ce09a02041e
KURASHIKI Satoru <lurdan@gmail.com>
2019-02-17 12:39:41 +0900

    update fortitude60 keymap

--- fortitude60/keymaps/lurdan/keymap.c
+++ fortitude60/keymaps/lurdan/keymap.c
@@ -39,6 +39,7 @@ enum custom_keycodes {
 #define RCTL_SPC RCTL_T(KC_SPC)
 #define ALT_MHEN RALT_T(KC_MHEN)
 #define ALT_HENK RALT_T(KC_HENK)
+#define SFT_BSLS RSFT_T(JP_BSLS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -59,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ESC_RAIS,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_YEN, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_MINS, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, JP_LBRC, JP_RBRC, KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, JP_LBRC, JP_RBRC, KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, SFT_BSLS, \
                  KC_LGUI,ALT_MHEN,LCTL_SPC, KC_BSPC,TAB_LOWR,ENT_LOWR, KC_SPC,  RCTL_SPC,RAISE,   ALT_HENK \
 ),
 
@@ -130,13 +131,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.             ,-----------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  |  F10 |  INS |
  * |------+------+------+------+------+------.             ,------+------+------+------+------+------|
- * |ZenHan|      |      |      |      |      |             |  F11 |  F12 |  ↑  |      |      |  DEL |
+ * |ZenHan|      |      |      |      |      |             |  F11 |  F12 |  ↑   |      |      |  DEL |
  * |------+------+------+------+------+------.             ,------+------+------+------+------+------|
- * | Ctrl |      |      |      |      |      |             |      |  ←  |  ↓  |  →  | HOME | PGUP |
+ * | Ctrl |      |      |      |      |      |             |      |  ←   |  ↓   |  →   | HOME | PGUP |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * | Shift|      |      |      |      |      |  Ins |  Del |      |      |  ↓  |      |  END | PGDN |
+ * | Shift|      |      |      |      |      |  Ins |  Del |      |      |  ↓   |      |  END | PGDN |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
- *               |  GUI |  Alt | Ctrl | Bksp |      |      |  Spc | Ctrl |      |  Alt |
+ *               |  GUI |AltMhn|CtlSpc| Bksp |      |      |  Spc |CtlSpc|      |AltHen|
  *               `---------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT( \
